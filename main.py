0d8a9f4ece9d15cf4c0b6acf454da02a61ff7656
ThanosVk <66605182+ThanosVk@users.noreply.github.com>
2021-06-25 21:04:26 +0300

    changes

--- main.py
+++ main.py
@@ -71,7 +71,22 @@ def chatbot_response(msg):
 
 
 # Creating GUI with tkinter
-def send(event):
+def send():
+    msg = EntryBox.get("1.0", 'end-1c').strip()
+    EntryBox.delete("0.0", END)
+
+    if msg != '':
+        ChatLog.config(state=NORMAL)
+        ChatLog.insert(END, "You: " + msg + '\n\n')
+        ChatLog.config(foreground="#442265", font=("Verdana", 12))
+
+        res = chatbot_response(msg)
+        ChatLog.insert(END, "Bot: " + res + '\n\n')
+
+        ChatLog.config(state=DISABLED)
+        ChatLog.yview(END)
+
+def send_withEnter(event):
     msg = EntryBox.get("1.0", 'end-1c').strip()
     EntryBox.delete("0.0", END)
 
@@ -100,14 +115,15 @@ ChatLog.config(state=DISABLED)
 scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
 ChatLog['yscrollcommand'] = scrollbar.set
 
+photo = PhotoImage(file = r"send.png")
 # Create Button to send message
-SendButton = Button(base, font=("Verdana", 12, 'bold'), text="Send", width="12", height=5,
-                    bd=0, bg="#32de97", activebackground="#3c9d9b", fg='#ffffff',
+SendButton = Button(base, font=("Verdana", 12, 'bold'), image=photo, width="12", height=5,
+                    bd=0, bg="#32c7de", activebackground="#3c9d9b", fg='#ffffff',
                     command=send)
 
 # Create the box to enter message
 EntryBox = Text(base, bd=0, bg="white", width="29", height="5", font="Arial")
-EntryBox.bind("<Return>", send)
+EntryBox.bind("<Return>", send_withEnter)
 
 
 # Place all components on the screen
